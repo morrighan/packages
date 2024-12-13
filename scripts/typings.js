1305ce2fe1b15e7a60490641689a33e77603c4f9
Yongbin Min <contact@cichol.com>
2024-12-13 17:21:45 +0900

    Refactor postbuild script file

---
+++ scripts/typings.js
@@ -0,0 +1,86 @@
+// Node.js built-in APIs.
+import { copyFile, rm as remove } from 'fs/promises';
+import path from 'path';
+import process from 'process';
+import { Worker, isMainThread, workerData } from 'worker_threads';
+
+// Third-party modules.
+import Promise from 'bluebird';
+import { execa } from 'execa';
+import { glob } from 'glob';
+import { oraPromise as ora } from 'ora';
+import { Extractor, ExtractorConfig } from '@microsoft/api-extractor';
+
+async function generateDeclarations(projectFolder = workerData) {
+    await execa({ cwd: projectFolder })`tsc --project sources --outDir types`;
+}
+
+async function mergeAllDeclarations(projectFolder = workerData) {
+    const files = [ 'package.json', 'tsconfig.json', 'types/index.d.ts', 'dists/index.d.ts' ]
+        .map(filename => path.resolve(projectFolder, filename));
+
+    const [ packageJsonFullPath, tsconfigFilePath, mainEntryPointFilePath, untrimmedFilePath ] = files;
+
+    const extractorConfig = ExtractorConfig.prepare({
+        configObject: {
+            projectFolder,
+            mainEntryPointFilePath,
+            compiler: { tsconfigFilePath },
+            dtsRollup: { enabled: true, untrimmedFilePath }
+        },
+
+        packageJsonFullPath
+    });
+
+    Extractor.invoke(extractorConfig, {
+        localBuild: true,
+        messageCallback: message => Object.assign(message, { logLevel: 'none' })
+    });
+}
+
+async function cloneFileForCommonJS(projectFolder = workerData) {
+    const original = path.resolve(projectFolder, 'dists/index.d.ts');
+    const destination = original.replace(/\.d\.ts$/, '.d.cts');
+
+    await copyFile(original, destination);
+}
+
+async function removeTemporaryFiles(projectFolder = workerData) {
+    const typesDirectory = path.resolve(projectFolder, 'types');
+
+    await remove(typesDirectory, { recursive: true, force: true });
+}
+
+export default async function main() {
+    if (isMainThread) {
+        const pattern = path.resolve(import.meta.dirname, '../packages/*');
+        const folderPaths = await glob(pattern);
+
+        const tasks = Promise.resolve(folderPaths)
+            .filter(projectFolder => (
+                path.basename(projectFolder) !== 'eslint-config'
+            ))
+            .map(projectFolder => new Promise((resolve, reject) => {
+                const worker = new Worker(import.meta.filename, { workerData: projectFolder });
+
+                worker.on('exit', exitCode => {
+                    if (exitCode === 0) {
+                        resolve();
+                    } else {
+                        reject(new Error('Unknown error occurred'));
+                    }
+                });
+            }));
+
+        await ora(tasks, 'Generating type declarations ...');
+    } else {
+        await generateDeclarations();
+        await mergeAllDeclarations();
+        await cloneFileForCommonJS();
+        await removeTemporaryFiles();
+    }
+}
+
+if (import.meta.filename === process.argv[1]) {
+    await main();
+}
