2ba8eed1592f0c8972e952a2507469dec35d72f9
Yongbin Min <contact@cichol.com>
2025-03-15 05:25:36 +0900

    Fix tests

--- packages/alias-mapper/tests/index.ts
+++ packages/alias-mapper/tests/index.ts
@@ -1,8 +1,6 @@
 // Node.js built-in APIs.
 import console from 'console'
-import fs from 'fs/promises'
 import path from 'path'
-import { serialize } from 'v8'
 
 // Third-party modules.
 import { test, expect } from 'vitest'
@@ -12,28 +10,30 @@ import * as Babel from '@babel/core'
 import { ESLint, loadESLint } from 'eslint'
 
 // Constants.
-const examplesPath = path.resolve(import.meta.dirname, 'examples')
-const targetFile = path.resolve(examplesPath, 'sources/frontend/components/button/index.js')
-const astFile = path.resolve(import.meta.dirname, 'artifacts/ast.data')
+const TARGET_ROOT = path.resolve(import.meta.dirname, 'examples')
+const TARGET_FILE = path.resolve(TARGET_ROOT, 'sources/frontend/components/button/index.js')
 
-// Artifacts.
-const savedAst = fs.readFile(astFile)
+//
+const eslint = await loadESLint({ useFlatConfig: true }).then(Linter => new Linter())
 
-test(`ESLint v${ESLint.version} should lint without an error`, async () => {
-	const linter = await loadESLint().then(Linter => new Linter())
-	const [ { messages } ] = await linter.lintFiles([ targetFile ])
+test(`should work properly w/ ESLint v${ESLint.version}`, async () => {
+	const [ result ] = await eslint.lintFiles([ TARGET_FILE ])
 
-	for (const { message, ruleId, line, column } of messages) {
-		console.error(`[${(ruleId ?? '?')}] ${message} (${line}:${column})`)
+	try {
+		expect(result, 'An error has not to be raised').to.have.own.property('errorCount', 0)
+	} catch {
+		console.table(result.messages)
 	}
-
-	// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-	expect(messages, 'An error has to be not raised').to.be.empty
 })
 
-test(`Babel v${Babel.version} should compile without an error`, async () => {
-	const fileResult = await Babel.transformFileAsync(targetFile, { root: examplesPath, code: false, ast: true })
-	const builtAst = fileResult && serialize(fileResult.ast)
+test(`should work properly w/ Babel v${Babel.version}`, async () => {
+	const result = Babel.transformFileAsync(TARGET_FILE, { root: TARGET_ROOT, compact: true })
 
-	expect(builtAst?.compare(await savedAst), 'The abstract syntax tree does not match').to.equal(0)
+	await expect(result, 'The built code does not match').resolves.to.have.own.property('code', [
+		'import"./../../../common/logger";',
+		'import{initialize}from"./../../../backend/database";',
+		'import{Account}from"./../../../backend/models/account";',
+		'import"./../checkbox";',
+		'export{Account,initialize};',
+	].join(''))
 })
