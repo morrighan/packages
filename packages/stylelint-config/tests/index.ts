03aa78d89ede2c08a1cb37c5f74c2988e221306e
Yongbin Min <contact@cichol.com>
2025-10-02 22:08:01 +0900

    Add new test case

--- packages/stylelint-config/tests/index.ts
+++ packages/stylelint-config/tests/index.ts
@@ -1,8 +1,12 @@
 // Third-party modules.
 import { test, expect } from 'vitest'
+import postcss from 'postcss'
+import stylelint from 'stylelint'
+import { version } from 'stylelint/package.json' with { type: 'json' }
 
 // Testing target.
 import PROPERTIES_LIST from '../properties'
+import config from '..'
 
 function indexOf(propertyName: string): number {
 	return PROPERTIES_LIST.includes(propertyName)
@@ -22,4 +26,10 @@ test('should have a properties list sorted as designed', () => {
 	//
 	expect(indexOf('font')).to.be.lessThan(indexOf('font-style'))
 	expect(indexOf('transform')).to.be.greaterThan(indexOf('transform-style'))
+})
+
+test(`should work properly w/ Stylelint v${version}`, async () => {
+	const result = postcss([ stylelint(config) ]).process('', { from: 'test.css' })
+
+	await expect(result).resolves.to.have.own.property('css')
 })
