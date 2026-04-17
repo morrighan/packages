cde84e6f91e41b300abbf4434b38e7be0486dcab
Yongbin Min <contact@cichol.com>
2026-04-18 03:33:55 +0900

    Make crypto functions to get msgpack instances

--- packages/cryptography/sources/index.ts
+++ packages/cryptography/sources/index.ts
@@ -1,6 +1,12 @@
 // Third-party modules.
 import invariant from 'tiny-invariant'
-import { encode, decode, type EncoderOptions, type DecoderOptions } from '@msgpack/msgpack'
+
+import {
+	encode as encodeFn,
+	decode as decodeFn,
+	Encoder,
+	Decoder,
+} from '@msgpack/msgpack'
 
 // Local helpers.
 import { ByteSize, Algorithm } from '#constants'
@@ -22,7 +28,7 @@ export async function generateKeys(): Promise<[ publicKey: ArrayBuffer, privateK
 export async function encrypt(
 	data: unknown,
 	publicKey: ArrayBuffer,
-	encoderOptions?: EncoderOptions,
+	encode: typeof encodeFn | Encoder = encodeFn,
 ): Promise<string> {
 	invariant(!!crypto.subtle)
 	invariant(publicKey instanceof ArrayBuffer)
@@ -35,8 +41,10 @@ export async function encrypt(
 		.then(sharedSecret => calculateHash(sharedSecret, salt))
 		.then(hashOfSecret => splitByChunkSizes(hashOfSecret, ByteSize.IV, ByteSize.ENCRYPTION_KEY))
 
+	const encodedData = encode instanceof Encoder ? encode.encode(data) : encode(data)
+
 	const encrypted = await crypto.subtle.importKey('raw', encryptionKey, Algorithm.AES_GCM, false, [ 'encrypt' ])
-		.then(cipherKey => crypto.subtle.encrypt({ ...Algorithm.AES_GCM, iv }, cipherKey, encode(data, encoderOptions)))
+		.then(cipherKey => crypto.subtle.encrypt({ ...Algorithm.AES_GCM, iv }, cipherKey, encodedData))
 
 	return base64FromBuffer(concatBuffers(shareableKey, salt.buffer, encrypted))
 }
@@ -44,7 +52,7 @@ export async function encrypt(
 export async function decrypt(
 	data: string,
 	privateKey: ArrayBuffer,
-	decoderOptions?: DecoderOptions,
+	decode: typeof decodeFn | Decoder = decodeFn,
 ): Promise<unknown> {
 	invariant(!!crypto.subtle)
 	invariant(typeof data === 'string')
@@ -60,5 +68,5 @@ export async function decrypt(
 	const decrypted = await crypto.subtle.importKey('raw', decryptionKey, Algorithm.AES_GCM, false, [ 'decrypt' ])
 		.then(cipherKey => crypto.subtle.decrypt({ ...Algorithm.AES_GCM, iv }, cipherKey, encrypted))
 
-	return decode(decrypted, decoderOptions)
+	return decode instanceof Decoder ? decode.decode(decrypted) : decode(decrypted)
 }
