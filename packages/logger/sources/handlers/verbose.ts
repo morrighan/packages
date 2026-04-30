9107797ee8fbb0cf3a0648c5723cf89656637f03
Yongbin Min <contact@cichol.com>
2026-05-01 01:00:04 +0900

    Desaturate text color on verbose level

--- packages/logger/sources/handlers/verbose.ts
+++ packages/logger/sources/handlers/verbose.ts
@@ -1,11 +1,20 @@
+// Third-party modules.
+import { gray } from '@std/fmt/colors'
+
 // Local helpers.
 import LoggingLevel from '#helpers/logging-level'
 import ColorScheme from '#helpers/color-scheme'
 import decorateLabel from '#helpers/label-decorator'
+
+// Type definitions.
+type LoggingData = { level: string, message: string } & Record<PropertyKey, any>
+type HandledData = { label: string, payload: string }
 
 // Handler metadata.
 export const name = 'verbose'
 export const level = LoggingLevel.Verbose
 export const label = decorateLabel('VERBOSE', ColorScheme.Loud)
 
-export default null
+export default function handle(data: LoggingData): HandledData | undefined {
+	return { label, payload: gray(data.message) }
+}
